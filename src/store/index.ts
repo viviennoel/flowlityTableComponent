9ea23884c74d63dfccbda950ee41eb9dc6170c37
viviennoel <52512132+viviennoel@users.noreply.github.com>
2021-12-19 23:53:54 +0100

    feat: store implementation

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,26 @@
 import { createStore } from 'vuex'
+import arrayData from '@/data/db.json'
+import analysisData from '@/data/analysis.json'
+import axios from "axios";
 
 export default createStore({
   state: {
+    data : arrayData.recordData[arrayData.recordData.length -1].data,
+    analysis : analysisData.analysisData[analysisData.analysisData.length -1].data,
+    updatePending: false
   },
   mutations: {
+    modifyData (state) {
+      state.updatePending = true;
+    },
+    exportData (state) {
+      axios.post("http://localhost:3001/recordData", 
+        {
+          update: new Date(),
+          data: state.data
+        }
+      );
+    }
   },
   actions: {
   },
