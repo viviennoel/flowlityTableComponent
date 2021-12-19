16416afebe31ff32e148fc9fb83454e3bac1cf77
viviennoel <52512132+viviennoel@users.noreply.github.com>
2021-12-20 00:01:12 +0100

    feat: creation component button reusable

---
+++ src/components/Button.vue
@@ -0,0 +1,13 @@
+<template>
+  <button @click="onClick" class="btn btn-primary">
+    <slot>Button</slot>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'MyButton',
+});
+</script>
