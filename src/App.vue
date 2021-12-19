54c28e3c53adbd8ab5345a587c14b6c5a5a2ab35
viviennoel <52512132+viviennoel@users.noreply.github.com>
2021-12-19 23:52:23 +0100

    feat: router structure

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header/>
   <router-view/>
 </template>
 
+<script setup>
+  import Header from "@/components/Header.vue";
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  @import "assets/style/designSystemFlowlity/v1.0/style.scss";
+  @import "assets/style/bootstrap/css/bootstrap.min.css";
 </style>
