b43bc38c9d7d9c21ee9983d78212bc56727d650e
viviennoel <52512132+viviennoel@users.noreply.github.com>
2021-12-20 02:25:36 +0100

    fix: comments & clean of  code

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header/>
+  <router-view/>
+</template>
+
+<script setup>
+  import Header from "@/components/Header.vue";
+</script>
+
+<style lang="scss">
+  @import "assets/style/designSystemFlowlity/v1.0/style.scss";
+  @import "assets/style/bootstrap/css/bootstrap.min.css";
+</style>
